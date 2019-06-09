6164aa8b40c11a385e8a51198dde0e3dc99fdfde
Shubhadeep Das <shubhadeep.das2016@vitstudent.ac.in>
2019-06-09 22:36:56 +0530

    Updated deps

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import IndecisonApp from "./App";
-import CounterApp from "./Counter";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<IndecisonApp />, document.getElementById("root"));
